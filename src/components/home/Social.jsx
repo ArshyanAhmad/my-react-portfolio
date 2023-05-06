a43f793da4eccf0d5f92974da053eb9fef4237ce
Arsh <arshahmed235@gmail.com>
2023-05-06 20:36:25 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Social() {
+    return (
+        <div className="home__social">
+            <a href="" className="home__social-icon" target='_blank'>
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="" className="home__social-icon" target='_blank'>
+                <i className="uil uil-dribbble"></i>
+            </a>
+
+            <a href="https://github.com/ArshyanAhmad" className="home__social-icon" target='_blank'>
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
